b78901b852f820b2c0fb603a3a4d9225d898d964
Sorin Chis <kish.sorin@yahoo.com>
2019-01-29 14:20:07 +0200

    kewdown event added on window object

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 // Your code goes here
 
-
 const h2 = document.querySelectorAll('h2');
 const imageRound = document.querySelector('.rounded');
 const container = document.querySelector('.home');
@@ -9,8 +8,7 @@ const h1 = document.querySelector('h1');
 //const contentSection=document.querySelector('.content-section');
 const title = document.querySelector('.titleDoc');
 
-
-//function that return a random color 
+//function that return a random color
 function getRandomColor() {
 	var letters = '0123456789ABCDEF';
 	var color = '#';
@@ -18,8 +16,7 @@ function getRandomColor() {
 		color += letters[Math.floor(Math.random() * 16)];
 	}
 	return color;
-} 
-
+}
 
 //random color for all h2 in the page when 'mouseover' on h2 -----  center h2 on click
 h2.forEach((item) => {
@@ -32,7 +29,6 @@ h2.forEach((item) => {
 		event.target.style.textAlign = 'center';
 	});
 });
-
 
 //added random color on all a tag when hover over container
 container.addEventListener('mousemove', colorRandom);
@@ -52,4 +48,30 @@ h1.addEventListener('mouseover', (event) => {
 h1.addEventListener('mouseout', (event) => {
 	event.target.textContent = 'Lambda School Fun Bus';
 	event.target.style.color = 'red';
-});
+}); 
+
+
+//adding a keydown event on window 
+window.addEventListener('keydown', checkKeyPress);
+
+const textContent=document.querySelectorAll('.text-content');
+console.log(textContent[0]);
+
+function checkKeyPress(key) {
+	if (key.keyCode == '65') {
+		alert("The 'a' letter key has been pressed.");
+    }
+    
+    if (key.keyCode == '82') {
+        textContent[0].style.backgroundColor='red';
+    }
+    if (key.keyCode == '87') {
+        textContent[0].style.backgroundColor='white';
+    }
+    if (key.keyCode == '66') {
+        textContent[0].style.backgroundColor='blue';
+    }
+    if (key.keyCode == '86') {
+        textContent[0].style.backgroundColor='violet';
+	}
+}
