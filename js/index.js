0df91ebeb95f77ab4b49b31725f163658453ae91
Sorin Chis <kish.sorin@yahoo.com>
2019-01-29 13:36:47 +0200

    project forked and cloned from github, selectors added for future use

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+
+const h2 = document.querySelectorAll('h2');
+const imageRound = document.querySelector('.rounded');
+const container = document.querySelector('.home');
+const aNav = document.querySelectorAll('a');
+const h1 = document.querySelector('h1');
+//const contentSection=document.querySelector('.content-section');
+const title = document.querySelector('.titleDoc');
