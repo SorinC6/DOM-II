e44c349bdafe4bb45af833156e5a874166ab7b32
Sorin Chis <kish.sorin@yahoo.com>
2019-01-29 13:47:14 +0200

    added function for getting a random color, added mouseover and click on h2

--- js/index.js
+++ js/index.js
@@ -8,3 +8,27 @@ const aNav = document.querySelectorAll('a');
 const h1 = document.querySelector('h1');
 //const contentSection=document.querySelector('.content-section');
 const title = document.querySelector('.titleDoc');
+
+
+//function that return a random color 
+function getRandomColor() {
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+} 
+
+
+//random color for all h2 in the page when 'mouseover' on h2 -----  center h2 on click
+h2.forEach((item) => {
+	item.addEventListener('mouseover', function() {
+		let c = getRandomColor();
+		item.style.color = c;
+		//item.style.color='green';
+	});
+	item.addEventListener('click', (event) => {
+		event.target.style.textAlign = 'center';
+	});
+});
